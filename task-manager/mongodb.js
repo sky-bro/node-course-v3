558863c4ab0c585e58500ab0484dbff192b5a3a2
sky <sky_bro@qq.com>
2019-05-14 09:37:56 +0800

    try create, read from db; callback & promise

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,7 +1,9 @@
 // CRUD creat read update delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+console.log(new Date());
+// return console.log(new ObjectID().getTimestamp());
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -10,6 +12,56 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     if (error){
         return console.log(`Unable to connect ${databaseName}!`);
     }
-    console.log('Connected correctly!');
+    console.log(`Connected correctly to ${databaseName}!`);
+    const db = client.db(databaseName);
+    // db.collection('users').insertOne({
+    //     name: 'sky',
+    //     age: 20
+    // }, (error, result) => {
+    //     if (error){
+    //         return console.log('unable to insert user');
+    //     }
+    //     console.log(result.ops);
+    // });
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'buy apples',
+            completed: true
+        },
+        {
+            description: 'go to bed early',
+            completed: false
+        },
+        {
+            description: 'review compilers',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error){
+            return console.log('unable to insert tasks');
+        }
+        console.log(result.ops);
+    });
+
+    db.collection('tasks').findOne({completed: false}, (error, result) => {
+        if (error){
+            return console.log(error);
+        }
+        console.log(result);
+    });
+
+    db.collection('tasks').find({completed:false}).count((error, count) => {
+        if (!error)
+            console.log(`got ${count} result`);
+    })
+
+    db.collection('tasks').find({completed:false}).toArray((error, tasks)=>{
+        if (!error)
+            console.log(tasks);
+    })
+
+
+
     client.close();
 })
---
+++ task-manager/playground/promises.js
@@ -0,0 +1,27 @@
+const doWorkCallback = (callback) => {
+    setTimeout(()=>{
+        callback('This is an error-callback', undefined);
+        callback(undefined, 'Success-Callback!');
+    }, 2000);
+}
+
+doWorkCallback((error, result) => {
+    if (error){
+        return console.log(error);
+    }
+    console.log(result);
+})
+
+const doWorkPromise = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        reject('This is an Error-Promise');
+        console.log('hello after reject!');
+        resolve('Success-Promise!');
+    }, 2000);
+})
+
+doWorkPromise.then(result=>{
+    console.log(result);
+}).catch(error=>{
+    console.log(error);
+})
